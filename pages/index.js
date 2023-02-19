2074702382bad27876f59198c4812e6d46885322
fl0r1n9 <florin12@gmx.de>
2023-02-19 19:20:18 +0100

    added table

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
-import Link from "next/link";
 import styles from "../styles/Home.module.css"
 import Head from "next/head";
-import Nav from "components/Nav"
 import DataTable from "@/components/DataTable";
 
 export default function Home() {
